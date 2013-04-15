07a59228ffc6a8d1809e53ccc610b33969c20524
Matt Tortolani <matt@doodlemoonch.com>
2013-04-15 11:30:36 +0200

    Fixed bug

--- jobs/twitter_user.rb
+++ jobs/twitter_user.rb
@@ -14,6 +14,8 @@ SCHEDULER.every '2m', :first_in => 0 do |job|
   http = Net::HTTP.new('api.twitter.com')
   response = http.request(Net::HTTP::Get.new("/1/users/show.json?screen_name=#{twitter_username}"))
   followers_count = JSON.parse(response.body)['followers_count']
+  friends_count = JSON.parse(response.body)['friends_count']
+  statuses_count = JSON.parse(response.body)['statuses_count']
   
   send_event('twitter_followers', current: followers_count)
   send_event('twitter_following', current: friends_count)

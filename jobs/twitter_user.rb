484609b6b347b993a567b5cd1b9e319ead420dac
Matt Tortolani <matt@doodlemoonch.com>
2013-04-15 09:57:19 +0200

    Use JSON api instead of scraping HTML

--- jobs/twitter_user.rb
+++ jobs/twitter_user.rb
@@ -1,27 +1,21 @@
 #!/usr/bin/env ruby
 require 'net/http'
+require 'json'
 
 # Track public available information of a twitter user like follower, follower
 # and tweet count by scraping the user profile page.
 
 # Config
 # ------
+
 twitter_username = ENV['TWITTER_USERNAME'] || 'foobugs'
 
 SCHEDULER.every '2m', :first_in => 0 do |job|
-  http = Net::HTTP.new("twitter.com", Net::HTTP.https_default_port())
-  http.use_ssl = true
-  response = http.request(Net::HTTP::Get.new("/#{twitter_username}"))
-  if response.code != "200"
-    puts "twitter communication error (status-code: #{response.code})\n#{response.body}"
-  else
-
-    tweets = /profile["']>[\n\t\s]*<strong>([\d.]+)/.match(response.body)[1].delete('.').to_i
-    following = /following["']>[\n\t\s]*<strong>([\d.]+)/.match(response.body)[1].delete('.').to_i
-    followers = /followers["']>[\n\t\s]*<strong>([\d.]+)/.match(response.body)[1].delete('.').to_i
-    
-    send_event('twitter_user_tweets', current: tweets)
-    send_event('twitter_user_followers', current: followers)
-    send_event('twitter_user_following', current: following)
-  end
+  http = Net::HTTP.new('api.twitter.com')
+  response = http.request(Net::HTTP::Get.new("/1/users/show.json?screen_name=#{twitter_username}"))
+  followers_count = JSON.parse(response.body)['followers_count']
+  
+  send_event('twitter_followers', current: followers_count)
+  send_event('twitter_following', current: friends_count)
+  send_event('twitter_tweets', current: statuses_count)
 end

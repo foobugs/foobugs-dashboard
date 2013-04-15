bf026f789f2b31448e4fb00136438721f3b4b94f
Matt Tortolani <matt@doodlemoonch.com>
2013-04-15 12:58:29 +0200

    Now using the twitter ruby gem

--- jobs/twitter_user.rb
+++ jobs/twitter_user.rb
@@ -1,6 +1,5 @@
 #!/usr/bin/env ruby
-require 'net/http'
-require 'json'
+require 'twitter'
 
 # Track public available information of a twitter user like follower, follower
 # and tweet count by scraping the user profile page.
@@ -10,14 +9,18 @@ require 'json'
 
 twitter_username = ENV['TWITTER_USERNAME'] || 'foobugs'
 
+Twitter.configure do |config|
+  config.consumer_key = YOUR_CONSUMER_KEY
+  config.consumer_secret = YOUR_CONSUMER_SECRET
+  config.oauth_token = YOUR_OAUTH_TOKEN
+  config.oauth_token_secret = YOUR_OAUTH_TOKEN_SECRET
+end
+
+twitter_username = ENV['TWITTER_USERNAME'] || 'foobugs'
+
 SCHEDULER.every '2m', :first_in => 0 do |job|
-  http = Net::HTTP.new('api.twitter.com')
-  response = http.request(Net::HTTP::Get.new("/1/users/show.json?screen_name=#{twitter_username}"))
-  followers_count = JSON.parse(response.body)['followers_count']
-  friends_count = JSON.parse(response.body)['friends_count']
-  statuses_count = JSON.parse(response.body)['statuses_count']
   
-  send_event('twitter_followers', current: followers_count)
-  send_event('twitter_following', current: friends_count)
-  send_event('twitter_tweets', current: statuses_count)
+  send_event('twitter_followers', current: Twitter.user(twitter_username).followers_count)
+  send_event('twitter_following', current: Twitter.user(twitter_username).friends_count)
+  send_event('twitter_tweets',    current: Twitter.user(twitter_username).statuses_count)
 end
